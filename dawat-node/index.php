43cb5e231c4c0ff2eb1660d8187e659df1eb075e
Adarsh Thakur <86471067+adarsh1006@users.noreply.github.com>
2021-10-05 15:11:08 +0530

    Add files via upload

---
+++ dawat-node/index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurent Dawat</title>
+    
+<link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&display=swap" rel="stylesheet">
+  
+</head>
+
+<body>
+    <header class="header">
+        <nav class="first-nav">
+            <ul class="nav-left-content">
+                <li><img src="email-12-32.png" width="15px" alt="error"></li>
+                <li>Email: info@Restaurant.Com</li>
+                <li><img src="phone-128.png" width="15px" alt=""></li>
+                <li>Telephone: +1800 234 5678</li>
+            </ul>
+            <ul class="nav-right-content">
+                <li><a href="facebooklink.com"><img src="facebook-128.png" width="12px" alt="error"></a></li>
+                <li><a href="facebooklink.com"><img src="twitter-128.png" width="12px" alt="error"></a></li>
+                <li><a href="facebooklink.com"><img src="dribbble-128.png" width="12px" alt="error"></a></li>
+                <li><a href="facebooklink.com"><img src="behance-128.png" width="12px" alt="error"></a></li>
+                <li><a href="facebooklink.com"><img src="rss-128.png" width="12px" alt="error"></a></li>
+                <li><button id="booknow">BOOK A TABLE</button></li>
+            </ul>
+        </nav>
+        <hr>
+
+
+        <nav  class="second-nav">
+            <ul class="second-left-nav">
+                <li><img src="main_logo.png" alt="error" srcset=""></li>
+            </ul>
+            <ul class="second-right-nav">
+                <li><a href="#Home">HOME</a></li>
+                <li><a href="#About">ABOUT US</a></li>
+                <li><a href="#Offer">OFFER</a></li>
+                <li><a href="#Menu">MENU</a></li>
+                <li><a href="#Team">TEAM</a></li>
+                <li><a href="#Blog">BLOG</a></li>
+                <li><a href="#Gallery">GALLERY</a></li>
+                <li><a href="#Contact">CONTACT</a></li>
+                <li><button><img src="search-13-128.png" width="15px" alt="error"></button></li>
+            </ul>
+
+        </nav>
+        <section class="first">
+            <h1>Welccome To</h1>
+            <p><b>DAWAT</b> CAFE & RESTAURANT</p>
+            <button class="firstpage">RESERVATION NOW
+            </button>
+        </section>
+
+    </header>
+    <main>
+        <section id="About" class="about">
+            <h1>Our Story</h1>
+            <div class="about-left">
+                <img src="https://pngimg.com/uploads/letter_L/letter_L_PNG92.png" width="50px" alt="">
+                <p>
+                    orem ipsum dolor sit amet consectetur adipisicing elit. Commodi maxime earum tempore cupiditate,
+                    beatae, blanditiis nihil doloribus recusandae rerum labore nulla accusantium atque ad, at provident
+                    sint delectus officia! Provident delectus incidunt nemo quia, reprehenderit sint dignissimos at
+                    dolor soluta veritatis maxime consequatur ratione illo perferendis
+                </p>
+
+            </div>
+            <div class="about-right">
+                ipsum dolor sit amet consectetur adipisicing elit. Fugiat fuga nesciunt officiis ut. Praesentium, porro.
+                Dignissimos cupiditate assumenda minima molestias quam, sapiente rerum sit ex soluta fugit quae eos,
+                eveniet officia nulla facilis a fugiat odit corrupti accusantium aperiam. Placeat est cupiditate ipsa
+                repudiandae suscipit delectus debitis dolores blanditiis aspernatur minima!
+
+            </div>
+            <div class="sign"><img src="about_sign.png" alt=""></div>
+        </section>
+        <section id="Offer" class="offers">
+            <h1>Special Package</h1>
+            <img class="separator-offer" src="separator.png" alt="">
+            <div class="main-image">
+                <img  src="blog-1.jpg" height="400px" width="700px" alt="">
+            </div>
+            <div class="written-offer">
+                <img src="Untitled.jpg" alt="">
+            </div>
+                <div class="centered">
+                    <h3 style="margin-bottom:10px;font-size: x-large; width: 339px;">40% off for 8"-12" pizza-Wed,Thu,Friday only </h1>
+                    <p style="width: 339px;">Pizza 12 inch +1 SideDish+1.5L Coke Bottle Only From 209.000</p>
+                    <button class="order-now">ORDER NOW</button>
+                </div>
+           
+        </section>
+        
+        <section id="Menu" class="menu-book">
+            <div class="menu">
+            <h1>Today's Menu</h1>
+            <img src="separator.png" alt="">
+            <section class="menu-buttons">
+                <button id="all" class="choose">
+                    ALL
+                </button>
+                <button id="breakfast"class="choose">
+                    BREAKFAST
+                </button>
+                <button id="lunch"class="choose">
+                    LUNCH
+                </button>
+                <button id="dinner"class="choose">
+                    DINNER
+                </button>
+                <button id="coffee"class="choose">
+                    COFFEE
+                </button>
+                <button id="snacks"class="choose">
+                    SNACKS
+                </button>
+            </section>
+            </div> 
+            <div class="book">
+             <h1>BOOK A TABLE NOW !</h1>
+             <button class="bookbutton" id="booknow">BOOK NOW</button>
+            </div>
+        </section>
+
+        <section id="Team" class="team">
+            <h1>Our Chefs</h1>
+            <img src="separator.png" alt="">
+        </section>
+        <section class="chef">
+            <div class="team1">
+                  <img class="team-img" src="team-1.jpg" alt="">
+                  <div style="position: absolute; top: 540% ;left: 15%;" class="social-chef-1">
+                      <ul class="social-chef-1" style="list-style: none; " >
+                          <li><a href=""><img src="facebook-128.png"width="15px" alt=""></a></li>
+                          <li><a href=""><img src="twitter-128.png"width="15px" alt=""></a></li>
+                          <li><a href=""><img src="behance-128.png"width="15px" alt=""></a></li>
+                          <li><a href=""><img src="insta.png"width="15px" alt=""></a></li>
+                          <li><a href=""><img src="dribbble-128.png"width="15px" alt=""></a></li>
+                      </ul>
+                  </div>
+                  <h4>Mark Angelila</h4>
+                  <p>Cras senunc massa quisque tempor dolor sit amet</p>
+            </div>
+            <div class="team2">
+               <img class="team-img" src="team-2.jpg" alt="">
+               <div style="position: absolute; top: 540% ;left: 35%;"class="social-chef-2">
+                <ul class="social-chef-2" style="list-style: none; ">
+                    <li><a href=""><img src="facebook-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="twitter-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="behance-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="insta.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="dribbble-128.png"width="15px" alt=""></a></li>
+                </ul>
+            </div>
+               <h4>Angel Meskat</h4>
+               <p>Cras senunc massa quisque tempor dolor sit amet</p>
+            </div>
+            <div class="team3">
+            <img class="team-img" src="team-3.jpg" alt="">
+            <div style="position: absolute; top: 540% ;left: 55%;"class="social-chef-3">
+                <ul class="social-chef-3"style="list-style: none; ">
+                    <li><a href=""><img src="facebook-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="twitter-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="behance-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="insta.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="dribbble-128.png"width="15px" alt=""></a></li>
+                </ul>
+            </div>
+            <h4>Jon Doe</h4>
+            <p>Cras senunc massa quisque tempor dolor sit amet</p>
+            </div>
+            <div class="team4">
+             <img class="team-img" src="team-4.jpg" alt="">
+             <div style="position: absolute; top: 540% ;left: 75%;"class="social-chef-4">
+                <ul class="social-chef-4 "style="list-style: none; ">
+                    <li><a href=""><img src="facebook-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="twitter-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="behance-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="insta.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="dribbble-128.png"width="15px" alt=""></a></li>
+                </ul>
+            </div>
+             <h4>Angel Di Maria</h4>
+             <p>Cras senunc massa quisque tempor dolor sit amet</p>
+            </div>
+            <div class="team5">
+              <img class="team-img" src="team-5.jpg" alt="">
+              <div style="position: absolute; top: 540% ;left: 95%;" class="social-chef-5">
+                <ul class="social-chef-5" style="list-style: none;">
+                    <li><a href=""><img src="facebook-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="twitter-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="behance-128.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="insta.png"width="15px" alt=""></a></li>
+                    <li><a href=""><img src="dribbble-128.png"width="15px" alt=""></a></li>
+                </ul>
+            </div>
+              <h4>Park Ji Sung</h4>
+              <p>Cras senunc massa quisque tempor dolor sit amet</p>
+            </div>
+        </section>
+
+        
+        <section id="Blog" class="news">
+            <h1>Latest News</h1>
+            <img src="separator.png" alt="">
+
+        </section>
+
+    </main>
+
+    <section class="last-slide" id="Gallery">
+        <div class="gallery">
+            <h1>Gallery</h1>
+            <img src="separator.png" alt="">
+ </div>
+ <footer>
+     <div class="info">
+          <ul>
+              <li><img src="main_logo.png" alt=""></li>
+              <li>20,floor, Queensland Victoria Building. 60 California Street California USA</li>
+              <li><a href="hello@dawat.com">hello@dawat.com</a></li>
+              <li><a href="call">+88 000 0000 1111</a></li>
+          </ul>
+     </div>
+     <div id="Contact" class="social">
+         <ul class='social-list'>
+             <li><a href="facebook.com"><img src="facebook-128.png" width="20px" alt=""></a></li>
+             <li><a href="facebook.com"><img src="twitter-128.png" width="20px" alt=""></a></li>
+             <li><a href="facebook.com"><img src="dribbble-128.png" width="20px" alt=""></a></li>
+             <li><a href="facebook.com"><img src="g.png" width="12px" width="20px" alt=""></a></li>
+             <li><a href="facebook.com"><img src="flickr-3-256.png" width="20px" alt=""></a></li>
+             <li><a href="facebook.com"><img src="pinterest-4-256.png" width="20px" alt=""></a></li>
+             <li><a href="skype.com"><img src="skype-6-256.png" width="20px" alt=""></a></li>
+          
+         </ul>
+         
+         
+
+     </div>
+     <button onclick="location.href='index.html';" class="repeat"><img src="arrow-138-128.png" width="15px" alt=""></button>
+        <hr style="border-top:0px white solid;">
+     <div class="bottom">
+         
+         <div class="bottom-left">
+           <ul class="bottom-list">
+               <li><a href="term.com">Teams Of Use</a></li>
+               <li><a href="privacy.com">Privacy Policy</a></li>
+               <li><a href="Sitemap.com">SiteMap</a></li>
+               <li><a href="contact">Contact</a></li>
+           </ul>
+         </div>
+         <div class="bottom-right">
+          &COPY; 2017 <p>Dawat Restaurant</p>  All Rights Reserved
+         </div>
+
+     </div>
+</footer>
+    </section>
+
+</body>
+
+</html>
